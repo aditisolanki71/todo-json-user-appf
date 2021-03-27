3d7a7bfad3c756c2fba6cede1103d5b3d559c847
Unknown <adtsolanki22@gmail.com>
2021-03-27 17:36:57 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import About from './components/pages/About'
 import Navbar from './components/layout/navbar'
+import NotFound from './components/pages/NotFound'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 function App() {
   return (
+    <Router>
     <div className="App">
     <h1>Hello REact</h1>
     <Navbar />
-    <Home/>
-    <Contact />
-    <About />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
     </div>
+    </Router>
   );
 }
 
--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,23 +1,23 @@
 import React from 'react'
-// import { Nav ,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
+import {Link ,NavLink} from 'react-router-dom'
 const Navbar = () => {
    return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">REact user</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">REact user</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <NavLink exact className="nav-link" aria-current="page" to="/">Home</NavLink>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">About</a>
+          <li className="nav-item">
+            <NavLink exact className="nav-link" aria-current="page" to="/about">About</NavLink>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Contact</a>
+          <li className="nav-item">
+            <NavLink exact className="nav-link" aria-current="page" to="/contact">Contact</NavLink>
           </li>
         </ul>
       </div>
