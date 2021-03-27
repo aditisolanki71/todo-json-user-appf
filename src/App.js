fe47630a9b7c3810fb7a56f467b0636a229500a0
Unknown <adtsolanki22@gmail.com>
2021-03-27 17:15:35 +0530

    Added Bootstrap nd React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import About from './components/pages/About'
+import Navbar from './components/layout/navbar'
 function App() {
   return (
     <div className="App">
     <h1>Hello REact</h1>
+    <Navbar />
+    <Home/>
+    <Contact />
+    <About />
     </div>
   );
 }
